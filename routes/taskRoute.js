59e4691232d6c1f51b3dd36f33b2650bc96b7a9e
Satyam Bansal <sbansal@grepruby.com>
2018-05-07 17:10:54 +0530

    api route name changes

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -3,8 +3,8 @@ const router=express.Router();
 const {getTasks,addTask,deleteTask,updateTaskStatus}=require('../controllers/taskController');
 
 router.get('/tasks',getTasks);
-router.post('/addTask',addTask);
-router.delete('/deleteTask',deleteTask);
+router.post('/task',addTask);
+router.delete('/task',deleteTask);
 router.put('/updateTaskStatus',updateTaskStatus);
 
 module.exports=router;
