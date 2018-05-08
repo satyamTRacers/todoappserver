1600e04a6a362d7d89bbe26709b6e30689e13927
Satyam Bansal <sbansal@grepruby.com>
2018-05-08 11:59:43 +0530

    code formatting changes

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,10 +1,10 @@
-const express=require('express');
-const router=express.Router();
-const {getTasks,addTask,deleteTask,updateTaskStatus}=require('../controllers/taskController');
+const express = require('express');
+const router = express.Router();
+const {getTasks,addTask,deleteTask,updateTaskStatus} = require('../controllers/taskController');
 
 router.get('/tasks',getTasks);
 router.post('/task',addTask);
 router.delete('/task',deleteTask);
 router.put('/updateTaskStatus',updateTaskStatus);
 
-module.exports=router;
+module.exports = router;
